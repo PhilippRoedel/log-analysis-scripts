8f458bf02307367903ce68b47bc6985c99a0e270
philipp <philipproedel@posteo.de>
2023-09-08 12:21:49 +0200

    task1 script counts server access per license, lists top ten

--- task1.php
+++ task1.php
@@ -1,29 +1,25 @@
 <?php
-//$logFilePath = './data/original.log';
+$logFilePath = './data/original.log';
 //$logFilePath = './data/shortened.log';
-$logFilePath = './data/fake.log';
-$lines = file($logFilePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-$lineArray = [];
-
+//$logFilePath = './data/fake.log';
+$handle = fopen($logFilePath, 'r');
 $licenseArray = [];
+$countedValues = [];
 
-foreach ($lines as $line) {
-    $lineArray[] = $line;
-    echo $line, PHP_EOL;
+while (($line = fgets($handle)) !== false) {
     $position1 = strpos($line, 'serial=');
     $position1 = $position1 + strlen('serial=');
-    echo $position1, PHP_EOL;
     $position2 = strpos($line, 'version=');
-    echo $position2, PHP_EOL;
     $strlen = $position2 -1 - $position1;
-    echo "string length: $strlen", PHP_EOL;
     $extractedString = substr($line, $position1,$strlen);
-    echo "extracted string: $extractedString", PHP_EOL;
     $licenseArray[] = $extractedString;
-    $countedValues = array_count_values($licenseArray);
-    arsort($countedValues);
-    echo "extracted array: ", PHP_EOL;
-    print_r($countedValues);
-    echo PHP_EOL;
 }
-print_r($lineArray);
+fclose($handle);
+
+$countedValues = array_count_values($licenseArray);
+arsort($countedValues);
+
+$firstTen = array_slice($countedValues, 0, 10);
+foreach ($firstTen as $value => $count) {
+    echo "License '$value' is accessing the server $count times\n";
+}
